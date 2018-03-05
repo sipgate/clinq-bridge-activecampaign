152ac730fc49f0d45c6186c43fc93656595087a2
Stefan Lange Hegermann <lange-hegermann@sipgate.de>
2018-03-05 17:19:44 +0100

    switched to version with caching

--- index.js
+++ index.js
@@ -1,59 +1,10 @@
 const Clinq = require("clinq-crm-bridge");
-const ActiveCampaign = require("activecampaign");
-
-var ac
-var cache = {}
-
-function map(input) {
-    var data =[];
-    Object.keys(input).forEach( key => {
-        var element = input[key];
-        if (typeof element.first_name !== "undefined") {
-            var p = element.phone.replace(/[^0-9\+]/ig,"")
-            p = p.replace(/^00/, "")
-            p = p.replace(/^\+/, "")
-            p = "+"+p.replace(/^0/, "49")
-            if (p != "+")
-            data.push({
-                "name": element.name,
-                "phoneNumbers": [{
-                    "label":"",
-                    "phoneNumber":p
-                }]
-            })
-        }
-    })
-    return data
-}
-
-var allContacts = [];
-var page=1;
-
-function fetchContacts(previous) {
-    return new Promise((resolve, reject) => {
-        if (previous != null && Object.keys(previous).length<6) {
-            return resolve(allContacts)
-        }
-        if (previous != null) {
-            var cnt = map(previous);
-            allContacts = allContacts.concat(cnt)
-        }
-        var req = {"ids":"all", "full":1, "page":page++, "sort": "id"}
-        return ac.api("contact/list", req).then((data) => {
-            resolve(fetchContacts(data))
-        })
-    })
-}
-
-function fetchAllContacts() {
-    return fetchContacts(null)
-}
+const AcClinq = require("./clinq-ac-adapter");
 
 const adapter = {
-    getContacts: function(apikey) {
-        ac = new ActiveCampaign("https://sipgate.api-us1.com", apikey);
-        return fetchAllContacts()
-    }
+	getContacts: async ({ apiKey, apiUrl }) => {
+        return AcClinq.getContactList(apiKey, apiUrl)
+	}
 };
-    
+
 Clinq.start(adapter);
