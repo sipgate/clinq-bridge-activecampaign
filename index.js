7b31a693d2854d32f70d3b4e40503ab898fb0241
Stefan Lange Hegermann <lange-hegermann@sipgate.de>
2018-03-08 12:35:09 +0100

    returns all contacts with phone numbers

--- index.js
+++ index.js
@@ -2,19 +2,10 @@ const Clinq = require("clinq-crm-bridge");
 const AcClinq = require("./clinq-ac-adapter");
 
 const adapter = {
-<<<<<<< HEAD
+
 	getContacts: async ({ apiKey, apiUrl }) => {
         return AcClinq.getContactList(apiKey, apiUrl)
 	}
 };
 
 Clinq.start(adapter);
-=======
-    getContacts: function({apiKey, apiUrl}) {
-        ac = new ActiveCampaign(apiUrl, apiKey);
-        return fetchAllContacts()
-    }
-};
-
-Clinq.start(adapter);
->>>>>>> a36adbb46373e5b7e9472199e14dbb14aad65c72
