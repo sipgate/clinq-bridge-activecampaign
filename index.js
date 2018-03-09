1f5a1de3d7afaa07b46f79c40b20dca886857b38
Stefan Lange Hegermann <lange-hegermann@sipgate.de>
2018-03-09 13:51:56 +0100

    cleanup

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Clinq = require("clinq-crm-bridge");
 const ClinqLoader = require("./lib/clinq-loader");
-const ClinqAdapterActiveCampaign = require("./clinq-adapter-ac");
-const clinqLoader = new ClinqLoader(ClinqAdapterActiveCampaign)
+const ClinqAdapter = require("./lib/adapters/clinq-adapter-ac");
+const clinqLoader = new ClinqLoader(ClinqAdapter)
 
 const adapter = {
 	getContacts: async ({ apiKey, apiUrl }) => {
